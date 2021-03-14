267afce9db48cfabdd054356b683af78967f2197
Karang <arthur.hennequin@free.fr>
2021-03-14 19:56:54 +0100

    Use item atlas

--- webpack.common.js
+++ webpack.common.js
@@ -64,9 +64,7 @@ const config = {
         { from: path.join(__dirname, '/node_modules/prismarine-viewer/public/textures/'), to: './textures/' },
         { from: path.join(__dirname, '/node_modules/prismarine-viewer/public/worker.js'), to: './' },
         { from: path.join(__dirname, '/node_modules/prismarine-viewer/public/supportedVersions.json'), to: './' },
-        { from: path.join(__dirname, 'assets/minecraftia.woff'), to: './' },
-        { from: path.join(__dirname, 'assets/mojangles.ttf'), to: './' },
-        { from: path.join(__dirname, 'assets/prismarinejs.ico'), to: './favicon.ico' },
+        { from: path.join(__dirname, 'assets/'), to: './' },
         { from: path.join(__dirname, 'extra-textures/'), to: './extra-textures/' }
       ]
     })
