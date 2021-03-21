c40b00ab5ce08bcd38ec30c7316f363ee52b062d
Romain Beaumont <romain.rom1@gmail.com>
2021-03-21 01:54:10 +0000

    add service worker for PWA

--- index.js
+++ index.js
@@ -18,6 +18,16 @@ const pathfinder = require('mineflayer-pathfinder')
 const { Vec3 } = require('vec3')
 global.THREE = require('three')
 
+if ('serviceWorker' in navigator) {
+  window.addEventListener('load', () => {
+    navigator.serviceWorker.register('/service-worker.js').then(registration => {
+      console.log('SW registered: ', registration)
+    }).catch(registrationError => {
+      console.log('SW registration failed: ', registrationError)
+    })
+  })
+}
+
 const maxPitch = 0.5 * Math.PI
 const minPitch = -0.5 * Math.PI
 
--- webpack.common.js
+++ webpack.common.js
@@ -2,6 +2,7 @@ const webpack = require('webpack')
 const path = require('path')
 const CopyPlugin = require('copy-webpack-plugin')
 const HtmlWebpackPlugin = require('html-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 // https://webpack.js.org/guides/production/
 
 const config = {
@@ -57,6 +58,13 @@ const config = {
       /prismarine-viewer[/|\\]viewer[/|\\]lib[/|\\]utils/,
       './utils.web.js'
     ),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+      include: ['index.html', 'manifest.json'] // not caching a lot as anyway this works only online
+    }),
     new CopyPlugin({
       patterns: [
         { from: path.join(__dirname, '/styles.css'), to: './styles.css' },
