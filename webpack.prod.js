163d9cecba873b6018b65456907034176fc991fd
Romain Beaumont <romain.rom1@gmail.com>
2021-11-12 21:04:09 +0000

    Revert "Make dev builds faster (#227)"
    This reverts commit 8c88ec638361e0fca6f3a770f3cbd4314b9f2433.
    
    Fix #240

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,30 +1,15 @@
 const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 
-const CopyPlugin = require('copy-webpack-plugin')
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
 const webpack = require('webpack')
-const WorkboxPlugin = require('workbox-webpack-plugin')
 
 module.exports = merge(common, {
   mode: 'production',
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
-    new LodashModuleReplacementPlugin(),
-    new CopyPlugin({
-      patterns: [
-        ...common[Symbol.for('webpack_directories')],
-        ...common[Symbol.for('webpack_files')]
-      ]
-    }),
-    new WorkboxPlugin.GenerateSW({
-      // these options encourage the ServiceWorkers to get in there fast
-      // and not allow any straggling "old" SWs to hang around
-      clientsClaim: true,
-      skipWaiting: true,
-      include: ['index.html', 'manifest.json'] // not caching a lot as anyway this works only online
-    })
+    new LodashModuleReplacementPlugin()
   ]
 })
