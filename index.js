3db8e30561058441db37112eca728b3d9c61ea66
sverben <59171289+sverben@users.noreply.github.com>
2022-07-24 15:19:36 +0200

    Add preload function to hud (required for already existing BossBars)

--- index.js
+++ index.js
@@ -187,6 +187,7 @@ async function connect (options) {
     noPongTimeout: 240 * 1000,
     closeTimeout: 240 * 1000
   })
+  hud.preload(bot);
 
   bot.on('error', (err) => {
     console.log('Encountered error!', err)
