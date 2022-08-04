322a011d7fa14eb159dde60adff766c891e65ec5
sverben <59171289+sverben@users.noreply.github.com>
2022-08-04 13:09:47 +0200

    Fix standard

--- modules/fetch.js
+++ modules/fetch.js
@@ -1,12 +1,10 @@
 const config = require('../config.json')
 
 let hasExtension
-let version
 if (chrome && chrome.runtime && config.extension_id) {
   try {
     chrome.runtime.sendMessage(config.extension_id, { type: 'version' }, response => {
       hasExtension = true
-      version = response.version
     })
   } catch (e) {
     hasExtension = false
