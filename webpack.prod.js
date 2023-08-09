08b9f48a34a3ccf150a1500d5fc398ea719564be
Vitaly <vital2580@icloud.com>
2023-08-09 18:36:27 +0300

    speed webpack builds

--- webpack.prod.js
+++ webpack.prod.js
@@ -3,6 +3,7 @@ const common = require('./webpack.common.js')
 
 const LodashModuleReplacementPlugin = require('lodash-webpack-plugin')
 const { CleanWebpackPlugin } = require('clean-webpack-plugin')
+const WorkboxPlugin = require('workbox-webpack-plugin')
 const webpack = require('webpack')
 
 module.exports = merge(common, {
@@ -10,6 +11,13 @@ module.exports = merge(common, {
   plugins: [
     new CleanWebpackPlugin(),
     new webpack.optimize.ModuleConcatenationPlugin(),
-    new LodashModuleReplacementPlugin()
+    new LodashModuleReplacementPlugin(),
+    new WorkboxPlugin.GenerateSW({
+      // these options encourage the ServiceWorkers to get in there fast
+      // and not allow any straggling "old" SWs to hang around
+      clientsClaim: true,
+      skipWaiting: true,
+      include: ['index.html', 'manifest.json'] // not caching a lot as anyway this works only online
+    }),
   ]
 })
