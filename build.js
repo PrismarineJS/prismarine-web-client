a67d05ae994e879454987f0ccf73d2ad75abcf13
Vitaly <vital2580@icloud.com>
2023-08-09 18:36:27 +0300

    speed webpack builds

---
+++ build.js
@@ -0,0 +1,20 @@
+const fsExtra = require('fs-extra')
+
+exports.copyFiles = () => {
+    fsExtra.copySync('node_modules/prismarine-viewer/public/blocksStates/', 'public/blocksStates/')
+    fsExtra.copySync('node_modules/prismarine-viewer/public/textures/', 'public/textures/')
+    fsExtra.copySync('node_modules/prismarine-viewer/public/worker.js', 'public/worker.js')
+    fsExtra.copySync('node_modules/prismarine-viewer/public/supportedVersions.json', 'public/supportedVersions.json')
+    fsExtra.copySync('assets/', 'public/')
+    fsExtra.copySync('extra-textures/', 'public/extra-textures/')
+    fsExtra.copySync('config.json', 'public/config.json')
+}
+
+exports.copyFilesDev = () => {
+    if (fsExtra.existsSync('public/config.json')) return
+    exports.copyFiles()
+}
+
+const fn = exports[process.argv[2]]
+
+if (fn) fn()
--- webpack.dev.js
+++ webpack.dev.js
@@ -2,15 +2,22 @@ const { merge } = require('webpack-merge')
 const common = require('./webpack.common.js')
 const path = require('path')
 
-module.exports = merge(common, {
+/** @type {import('webpack-dev-server').Configuration['rel']} */
+module.exports = merge(common,
+  /** @type {import('webpack').Configuration} */
+  {
   mode: 'development',
   devtool: 'inline-source-map',
   cache: true,
   devServer: {
-    contentBase: path.resolve(__dirname, './public'),
+    // contentBase: path.resolve(__dirname, './public'),
     compress: true,
-    inline: true,
+    // inline: true,
     // open: true,
-    hot: true
+    hot: true,
+    // liveReload: true,
+    devMiddleware: {
+      writeToDisk: true,
+    },
   }
 })
