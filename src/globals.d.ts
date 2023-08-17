2a2d7f3ec05a9409f366f276588efa5961aed747
Vitaly <vital2580@icloud.com>
2023-08-18 02:49:04 +0300

    remove duplicated types files & loose some other types

--- src/globals.d.ts
+++ src/globals.d.ts
@@ -4,6 +4,11 @@ declare interface Document {
     getElementById(id): any
 }
 
+declare interface DocumentFragment {
+    getElementById(id): HTMLElement & Record<string, any>
+    querySelector(id): HTMLElement & Record<string, any>
+}
+
 declare interface Window extends Record<string, any> {
 
 }
