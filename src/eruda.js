e04fef0e17d47e097acafcd2eadd06354a05060e
Vitaly <vital2580@icloud.com>
2023-09-04 09:23:23 +0300

    enable eruda only on mobile

--- src/eruda.js
+++ src/eruda.js
@@ -1,4 +1,6 @@
-if (process.env.NODE_ENV === 'development') {
+import { isMobile } from './menus/components/common'
+
+if (process.env.NODE_ENV === 'development' && isMobile()) {
   require('eruda').default.init()
   console.log('JS Loaded in', Date.now() - window.startLoad)
 }
