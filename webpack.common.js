d441efaef504a22b31f7d5eca8af9a5591f22e32
Romain Beaumont <romain.rom1@gmail.com>
2023-12-31 17:48:10 +0100

    avoid using @azure/msal-node (#358)
    * avoid using @azure/msal-node
    
    * Update webpack.common.js
    
    * Update webpack.common.js
    
    * Update webpack.common.js

--- webpack.common.js
+++ webpack.common.js
@@ -21,7 +21,8 @@ const config = {
       express: false,
       net: 'net-browserify',
       fs: false,
-      jose: false
+      jose: false,
+      '@azure/msal-node': false
     },
     fallback: {
       jose: false,
@@ -74,7 +75,6 @@ const config = {
         { from: path.join(__dirname, '/node_modules/prismarine-viewer/public/blocksStates/'), to: './blocksStates/' },
         { from: path.join(__dirname, '/node_modules/prismarine-viewer/public/textures/'), to: './textures/' },
         { from: path.join(__dirname, '/node_modules/prismarine-viewer/public/worker.js'), to: './' },
-        { from: path.join(__dirname, '/node_modules/prismarine-viewer/public/supportedVersions.json'), to: './' },
         { from: path.join(__dirname, 'assets/'), to: './' },
         { from: path.join(__dirname, 'extra-textures/'), to: './extra-textures/' },
         { from: path.join(__dirname, 'config.json'), to: './config.json' }
